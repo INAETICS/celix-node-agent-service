6e17c7c3b3f5ddc2148db3f61af089ea927afd5b
Pepijn Noltes <pepijnnoltes@gmail.com>
2015-11-26 11:07:16 +0100

    Add comment in build.sh to trigger cagent build for update celix branch

--- build.sh
+++ build.sh
@@ -23,3 +23,4 @@ git clone https://github.com/INAETICS/node-wiring-c.git
 cd node-wiring-c; ../cagent_builder.sh make_bundles
 # Example how to create a bundle
 # cd example_bundle; ../cagent_builder.sh make_bundles
+#
