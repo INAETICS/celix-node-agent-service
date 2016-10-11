6d0746b9af2724b732a56c5805359b1d02d34d8b
Pepijn Noltes <pepijnnoltes@gmail.com>
2016-10-11 11:26:56 +0200

    Updates standalone setup and updated travis config

--- celix-standalone-fedora-builder/main.sh
+++ celix-standalone-fedora-builder/main.sh
@@ -1,7 +1,6 @@
 #!/bin/bash
 
-#TODO optional strip executables/libs
-mkdir /root/docker-image
-dockerize -n -o /root/docker-image --add-file /usr/lib64/libjansson.so.4 /lib64/libjansson.so.4 /usr/local/bin/celix /usr/bin/gdbserver
+#TODO strip libs/executable and create a non stripped mirrot with gdb?
+
 cd /root/docker-image
 tar c --to-stdout *
